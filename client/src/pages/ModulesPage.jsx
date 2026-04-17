a5e652015439e4725a3451a2f581267c30172bbd
Aldina Brdar <aldina@kth.se>
2026-04-18 00:22:42 +0200

    Fully done but not adapted to MVP layers

--- client/src/pages/ModulesPage.jsx
+++ client/src/pages/ModulesPage.jsx
@@ -1,52 +1,102 @@
-//import { useState } from "react"
+import { useState } from "react"
 import "../styles/modulePageStyle.css"
+
+import {
+  ClockIcon,
+  WeatherIcon,
+  CalendarIcon,
+  TransportIcon,
+  NewsFeedIcon,
+  DailyComplimentsIcon,
+  NotesIcon,
+  ModulesIcon,
+} from '../views/Icons'
+
+function iconModule (module){
+  switch (module) {
+    case "Clock":
+      return <ClockIcon />
+    case "Weather":
+      return <WeatherIcon />
+    case "Calendar":
+      return <CalendarIcon />
+    case "SL Departures":
+      return <TransportIcon />
+    case "News Feed":
+      return <NewsFeedIcon />
+    case "Daily Compliments":
+      return <DailyComplimentsIcon />
+      case "Notes":
+        return <NotesIcon />
+    default:
+      return null
+  }
+}
 
 function ModulesPage() {
 
-  const module = [
+  const [module, setModuleToggle] = useState ([
     {
       moduleID: 1,
       name: "Clock",
       category: "core",
       description: "Display current time and date",
+      activeToggle: true,
     },
     {
       moduleID: 2,
       name: "Weather",
       category: "weather",
       description: "Current weather and forecast",
+      activeToggle: true,
     },
     {
       moduleID: 3,
       name: "Calendar",
       category: "productivity",
       description: "Google Calendar events",
+      activeToggle: true,
     },
     {
       moduleID: 4,
       name: "SL Departures",
       category: "transport",
       description: "Real-time Stockholm transport",
+      activeToggle: true,
     },
     {
       moduleID: 5,
       name: "News Feed",
       category: "productivity",
       description: "Latest news headlines",
+      activeToggle: false,
     },
     {
       moduleID: 6,
       name: "Daily Compliments",
       category: "core",
       description: "Random compliments to start your day",
+      activeToggle: false,
     },
     {
       moduleID: 7,
       name: "Notes",
       category: "productivity",
       description: "Important reminders and thoughts",
+      activeToggle: false,
     },
-  ]
+  ])
+
+  function toggleModules (moduleID){
+    setModuleToggle((previousState) => previousState.map((module) => {
+    if (module.moduleID === moduleID) 
+    return {...module, activeToggle: !module.activeToggle } //return new array with toggle change (toggled)
+
+      else {
+        return module //same content as before (not toggled)
+      }
+    }))
+  }
 
   return (
     <div className="page">
@@ -57,27 +107,51 @@ function ModulesPage() {
 
       <div className="content-panel">
         <div className="module-modules-description">
-        <h2>Available modules</h2>
-        <div className="module-p-text">
-        <p>Toggle modules on or off to customize your SmartMirror display.</p>
+        <div className="module-icon-header">
+          <ModulesIcon />
+        <h2>Available Modules</h2>
         </div>
+
+        <p>Toggle modules on or off to customize your SmartMirror display.</p>
         </div>
         <div className="modules">
 
           {module.map((module) => (
             <div className="module-box" key={module.moduleID}>
-              <div className="module-icon"></div>
-              <div className="module-title-description">
+              <div className="module-icon"> {iconModule(module.name)}</div>
+              <div className="module-description">
+              <div className="module-title-category">
               <h3>{module.name}</h3>
+              <div className={"module-category " + module.category}>{module.category}</div>
+              </div>
               <p>{module.description}</p>
               </div>
+
+              <div className="module-toggle-box">
+              <div>
+                {module.activeToggle ? (<div className="module-active">Active</div>) : null }
+              </div>
+
+              <label className="switch">
+                <input type="checkbox"
+                      checked={module.activeToggle}
+                      onChange={() => toggleModules(module.moduleID)}
+                />
+                <span className="slider round"></span>
+              </label>
+
             </div>
+            </div>
+            
             
           ))}
 
         </div>
-
       </div>
+
+      <div className="module-note">
+          <strong className="note-style">Note:</strong> Module changes are saved to your Firebase Firestore database and will be synced to your MagicMirror in real-time.
+        </div>
     </div>
   )
 }
--- client/src/views/Icons.jsx
+++ client/src/views/Icons.jsx
@@ -121,3 +121,57 @@ export function ModuleSmallIcon() {
     </svg>
   )
 }
+
+export function ClockIcon(){
+  return (
+  <svg width="16" height="16" viewBox="0 0 24 24" fill="none" 
+  stroke="currentColor" stroke-width="1.8" stroke-linecap="round" 
+  stroke-linejoin="round"><circle cx="12"
+   cy="12" r="10"/><path d="M12 6v6l4 2"/>
+   </svg>
+  )
+}
+
+export function WeatherIcon(){
+  return (
+    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" 
+    stroke="currentColor" stroke-width="1.8" stroke-linecap="round" 
+    stroke-linejoin="round"><path d="M12 2v2"/><path d="m4.93 4.93 1.41 1.41"/>
+    <path d="M20 12h2"/><path d="m19.07 4.93-1.41 1.41"/><path d="M15.947 12.65a4 4 0 0 0-5.925-4.128"/>
+    <path d="M13 22H7a5 5 0 1 1 4.9-6H13a3 3 0 0 1 0 6Z"/>
+    </svg>
+  )
+}
+
+export function NewsFeedIcon(){
+  return (
+    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" 
+    stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" >
+      <path d="M15 18h-5"/><path d="M18 14h-8"/>
+      <path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-4 0v-9a2 2 0 0 1 2-2h2"/>
+      <rect width="8" height="4" x="10" y="6" rx="1"/>
+      </svg>
+  )
+}
+
+export function DailyComplimentsIcon(){
+  return (
+    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" 
+    stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M11 14h2a2 2 0 0 0 0-4h-3c-.6 0-1.1.2-1.4.6L3 16"/>
+      <path d="m14.45 13.39 5.05-4.694C20.196 8 21 6.85 21 5.75a2.75 2.75 0 0 0-4.797-1.837.276.276 0 0 1-.406 0A2.75 2.75 0 0 0 11 5.75c0 1.2.802 2.248 1.5 2.946L16 11.95"/>
+      <path d="m2 15 6 6"/><path d="m7 20 1.6-1.4c.3-.4.8-.6 1.4-.6h4c1.1 0 2.1-.4 2.8-1.2l4.6-4.4a1 1 0 0 0-2.75-2.91"/>
+      </svg>
+  )
+}
+
+export function NotesIcon (){
+  return (
+    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" 
+    stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M13.4 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7.4"/>
+      <path d="M2 6h4"/><path d="M2 10h4"/><path d="M2 14h4"/><path d="M2 18h4"/>
+      <path d="M21.378 5.626a1 1 0 1 0-3.004-3.004l-5.01 5.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z"/>
+      </svg>
+  )
+}
