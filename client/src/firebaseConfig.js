ee64c7b456182157ef4ba6171a4ce1893631ec3c
Harry Lazaridis <harry.laza@outlook.com>
2026-04-20 13:48:06 +0200

    Delete client/src/firebaseConfig.js

--- client/src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDjY_VrwXKd37D17FhMRj0S8Bouu8qQU-k",
-  authDomain: "magic-mirror-e5482.firebaseapp.com",
-  projectId: "magic-mirror-e5482",
-  storageBucket: "magic-mirror-e5482.firebasestorage.app",
-  messagingSenderId: "733732733390",
-  appId: "1:733732733390:web:851708d26e0316da13eb9f"
-};
